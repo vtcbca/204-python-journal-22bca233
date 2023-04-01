64dc34e4229c0a92ba81d2ad88dc2628231ae051
22bca233 <127925449+22bca233@users.noreply.github.com>
2023-04-01 10:37:32 +0530

    Delete python solution directory

--- python solution/countvowel.py
+++
@@ -1,8 +0,0 @@
-#python script to enter any string and count vowels
-c=input("Enter any string :")
-count=0
-list=['a','e','i','o','u','A','E','I','O','U']
-for i in c:
-    if(i in list):
-        count+=1
-print("Tne number of vowels in entered string is :",count)
--- python solution/strpalindrome.py
+++
@@ -1,7 +0,0 @@
-#python script to check string is palindrome or not
-a=input("Enter any string :")
-b=a[::-1]
-if(a==b):
-    print("String is palindrome !")
-else:
-    print("String is not palindrome !")
